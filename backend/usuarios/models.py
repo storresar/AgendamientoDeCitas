d07b88c28c32aaa181cf843b7b822207ff15c825
Jondo <slim.shady99q@gmail.com>
2021-05-12 15:42:46 -0500

    Creacion de usuario y sus tipos

--- backend/usuarios/models.py
+++ backend/usuarios/models.py
@@ -1,4 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
-class usuario (models.Model):
+
+class tipo_usuario(models.Model):
+    nombre_rol = models.TextField(max_length=50)
+
+class usuario(AbstractUser):
+    TIPO_ROL = (
+        (1, 'Paciente'),
+        (2, 'Funcionario'),
+        (3, 'Administrador'),
+    )
+
+    rol = models.ForeignKey(tipo_usuario, on_delete=models.CASCADE)
+
