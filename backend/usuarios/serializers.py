5aa1de3292070ae351a3d755c9d11d6fcdb67f8d
Jondo <slim.shady99q@gmail.com>
2021-05-22 09:22:47 -0500

    Envio correo en la creacion de usuario

--- backend/usuarios/serializers.py
+++ backend/usuarios/serializers.py
@@ -1,7 +1,7 @@
-from django.db.models import fields, query_utils
 from rest_framework import serializers
-from .models import usuario, tipo_usuario
+from .models import usuario
 from django.contrib.auth.hashers import make_password
+from django.core.mail import send_mail
 
 
 class usuario_serializer(serializers.ModelSerializer):
@@ -11,9 +11,15 @@ class usuario_serializer(serializers.ModelSerializer):
         fields = ('id','username','password','first_name', 'last_name', 'email','fecha_nacimiento','rol')
 
     def create(self, validated_data):
-        nuevo = usuario.objects.create(
-            password = make_password(validated_data.pop('password')),
-            **validated_data
-        )
+        mensaje = 'Felicidades! Usted se ha registrado exitosamente en Sophy hostpital.\n'
+        mensaje += 'A continuacion mostaremos sus credenciales, por favor no las difunda con nadie mas.\n'
+        mensaje += 'Usuario:  ' + validated_data['username'] + '\n'
+        mensaje += 'Contraseña:  ' + validated_data['password'] + '\n'
+
+        if send_mail(subject='Creacion de cuenta',message=mensaje,from_email=None,recipient_list=[validated_data['email']]) > 0:
+            nuevo = usuario.objects.create(
+                password = make_password(validated_data.pop('password')),
+                **validated_data
+            )
         return nuevo
     
